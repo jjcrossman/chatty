6706868a80fcb1cc60833c2accfcb54a7d85ffee
JJ Crossman <jj.crossman@gmail.com>
2016-09-21 17:41:46 -0500

    finished project

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,17 +2,28 @@
 
 angular.module('chattyApp')
   .controller('MainCtrl', function ( $scope, messageService ) {
+
     messageService.getMessages().then(function ( response ) {
       $scope.messages = response.data;
+      $scope.messages = $scope.messages.reverse();
     });
 
-    $scope.addMessage = function ( message ) {
-      if (message) {
-        messageService.addMessage(message).then(function ( response ) {
+    $scope.addMessage = function ( message, user ) {
+      if ( message, user && $scope.currentUser !== "none" ) {
+        messageService.addMessage(message, user).then(function ( response ) {
           $scope.messages = response.data;
+          $scope.messages = $scope.messages.reverse();
         });
       }
     };
+
+    $scope.currentUser = "none";
+
+    $scope.setUserName = ( username ) => {
+       $scope.currentUser = username;
+    };
+
+    $scope.userName = "";
 
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//init
 const express = require( "express" );
 const { json } = require( "body-parser" );
 
@@ -8,11 +9,60 @@ app.use( json() );
 
 
 //database
-const messages = [];
+
+const timeObj = new Date();
+const messages = [
+  {
+      "author": "Bobby"
+      , "content": "Hey idiots, ya dead!"
+      , "time": new Date()
+  }
+  , {
+    "author": "Howard"
+    , "content": "Sometimes I'm here and that's about it."
+    , "time": new Date()
+  }
+];
 
 //REST methods
-app.get( "/api/messages", ( req, res ) => {
-  messages.push( req.body.message );
+app.get( '/api/messages', function( req, res ) {
+
+  res.status(200).set( {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  } ).send(JSON.stringify( messages ));
+
+} );
+
+app.post( "/api/messages", ( req, res ) => {
+
+  let newMessage = {
+    "author": `${ req.body.user }`
+    , "content": `${ req.body.message }`
+    , "time": new Date()
+  }
+  messages.push( newMessage );
+  res.status( 200 ).set( {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  } ).send( JSON.stringify( messages ) );
+} );
+
+//Options
+app.options( "/api/messages", ( req, res ) => {
+  res.set( { 'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept' } ).send();
 } );
 
 //listen
