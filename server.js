265e6d5c85b783629bdae17fc7507166ba916819
JJ Crossman <jj.crossman@gmail.com>
2016-09-21 15:39:30 -0500

    beginning setup of server for chatty app

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require( "express" );
+const { json } = require( "body-parser" );
 
+const app = express();
+const port = 8899;
+
+app.use( json() );
+
+
+//database
+const messages = [];
+
+//REST methods
+app.get( "/api/messages", ( req, res ) => {
+  messages.push( req.body.message );
+} );
+
+//listen
+
+
+app.listen( port, () => console.log( `Chatty is listening on port: ${ port }` ) );
